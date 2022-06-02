6fd4db8f9d46255ebd5b6372b0aac4e605bb2104
Jacob Hood <jacobwhood@gmail.com>
2022-06-02 14:38:02 -0700

    Update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ export default function App() {
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        I am Jacob! Howdy world.
         </div>
 
         <button className="waveButton" onClick={wave}>
